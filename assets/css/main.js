0c96fac38d66e376a95edbb00d9b2da48719bfde
alecasentini <ale.casentini@gmail.com>
2023-04-14 15:09:08 +0200

    Preparazione Snack #3

--- assets/css/main.js
+++ assets/css/main.js
@@ -75,3 +75,11 @@ let squadreFalliSubiti = squadre.map(({nome, falli}) => ({nome, falli}));
 console.log(squadre)
 console.log(squadreFalliSubiti);
 
+//========================================================================================
+/*      Snack 3
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
+se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+Usiamo i nuovi metodi degli array foreach o filter.*/
+
