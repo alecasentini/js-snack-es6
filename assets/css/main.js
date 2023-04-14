4a35acffe43614479ba6e294ea5ebc610a4ae4ec
alecasentini <ale.casentini@gmail.com>
2023-04-14 14:14:02 +0200

    Aggiunta Array biciclette

---
+++ assets/css/main.js
@@ -0,0 +1,16 @@
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal*/
+
+// creo array con biciclette
+const biciclette = [
+    { nome: "Bianchi", peso: 6 },
+    { nome: "Specialized", peso: 7 },
+    { nome: "Trek", peso: 8 },
+    { nome: "Cannondale", peso: 6.5 },
+    { nome: "Pinarello", peso: 6.2 },
+    { nome: "Colnago", peso: 6.8 },
+    { nome: "Scott", peso: 7.5 },
+    { nome: "Giant", peso: 8.2 },
+  ];
