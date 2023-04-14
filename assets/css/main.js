1ed86d4df939b37ebd393016079390b549142ddb
alecasentini <ale.casentini@gmail.com>
2023-04-14 14:37:01 +0200

    Creo array con squadre di calcio

--- assets/css/main.js
+++ assets/css/main.js
@@ -36,3 +36,26 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+// creo array con squadre di calcio
+let squadre = [
+    { nome: 'Juventus', punti: 0, falli: 0 },
+    { nome: 'Inter', punti: 0, falli: 0 },
+    { nome: 'Milan', punti: 0, falli: 0 },
+    { nome: 'Napoli', punti: 0, falli: 0 },
+    { nome: 'Roma', punti: 0, falli: 0 },
+    { nome: 'Atalanta', punti: 0, falli: 0 },
+    { nome: 'Lazio', punti: 0, falli: 0 },
+    { nome: 'Fiorentina', punti: 0, falli: 0 },
+    { nome: 'Sampdoria', punti: 0, falli: 0 },
+    { nome: 'Torino', punti: 0, falli: 0 },
+    { nome: 'Cagliari', punti: 0, falli: 0 },
+    { nome: 'Bologna', punti: 0, falli: 0 },
+    { nome: 'Genoa', punti: 0, falli: 0 },
+    { nome: 'Udinese', punti: 0, falli: 0 },
+    { nome: 'Bari', punti: 0, falli: 0 },
+    { nome: 'Palermo', punti: 0, falli: 0 },
+    { nome: 'Parma', punti: 0, falli: 0 },
+    { nome: 'Vicenza', punti: 0, falli: 0 }
+  ];
+  
