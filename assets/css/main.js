6931fea09b2dddb7570125716e7af9e5b90b2711
alecasentini <ale.casentini@gmail.com>
2023-04-14 15:54:11 +0200

    Soluzione con filter snack 3

--- assets/css/main.js
+++ assets/css/main.js
@@ -78,8 +78,26 @@ console.log(squadreFalliSubiti);
 //========================================================================================
 /*      Snack 3
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
-se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
+l'array generato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
+se come numeri ad esempio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter.*/
 
+// creo array di nomi
+const nomi = [
+    "Alex",
+    "Ginevra",
+    "Edoardo",
+    "Lucia",
+    "Riccardo"
+]
+
+// soluzione con filter
+function valoreIntermedio(array, a, b) {
+    return array.filter((value, index) => 
+        index > a && index < b
+    );
+}
+
+const risultato = valoreIntermedio(nomi, 0, 4);
+console.log(risultato)
