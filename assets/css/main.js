cd1a302853d3bd4956b38e01541bba586b646198
alecasentini <ale.casentini@gmail.com>
2023-04-14 14:24:35 +0200

    preparazione Snack #2

--- assets/css/main.js
+++ assets/css/main.js
@@ -1,4 +1,4 @@
-/*Snack 1
+/*    Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal*/
@@ -28,3 +28,11 @@ const { nome, peso } = biciLeggera;
 // Stampo la bici leggera a schermo
 const pesoMinore = document.getElementById("peso-minore");
 pesoMinore.innerHTML = `La bici più leggera è la ${nome}, che pesa solo ${peso} kg!`;
+
+//========================================================================================
+/*     Snack 2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
